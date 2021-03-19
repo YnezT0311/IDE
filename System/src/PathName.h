3356802b26405646ae2feb5f1a739fa3809d98da
YnezT0311 <59566630+YnezT0311@users.noreply.github.com>
2021-03-19 15:21:03 +0800

    Delete System directory

--- System/src/PathName.h
+++
@@ -1,8 +0,0 @@
-//#ifndef PATHNAME_H
-//#define PATHNAME_H
-
-//#include <string>
-//const std::string PATH = "/Users/ty/Documents/Project/Files/";
-//const std::string NAME = "arithemetic";      // 无后缀
-
-//#endif // PATHNAME_H
